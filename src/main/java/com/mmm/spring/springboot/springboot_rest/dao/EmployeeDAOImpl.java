04278f8cac28d8a30291cd0eaf5eece9a739ca75
Maksym <maksym.murza@gmail.com>
2021-08-17 11:23:56 +0300

    realization of DAOImpl without direct using of Hibernate

--- src/main/java/com/mmm/spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
+++ src/main/java/com/mmm/spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
@@ -1,15 +1,87 @@
+//this version of DAOImpl uses hibernate realization of JPA
+//package com.mmm.spring.springboot.springboot_rest.dao;
+//
+//import com.mmm.spring.springboot.springboot_rest.entity.Employee;
+//import org.hibernate.Session;
+//import org.hibernate.query.Query;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Repository;
+//
+//import javax.persistence.EntityManager;
+//import java.util.List;
+//
+//
+//@Repository
+//public class EmployeeDAOImpl implements EmployeeDAO{
+//
+//    @Autowired
+//    private EntityManager entityManager;
+//
+//    @Override
+////    @Transactional // this annotation used here if we have NO service layer
+//    public List<Employee> getAllEmployees() {
+//        Session session = entityManager.unwrap(Session.class);
+////        //all in one statement
+////        List <Employee> allEmployees = session
+////                .createQuery("from Employee ", Employee.class)
+////                .getResultList();
+//
+//        //the same as above in two statements
+//        Query <Employee> query = session
+//                .createQuery("from Employee ", Employee.class);
+//        List <Employee> allEmployees = query
+//                .getResultList();
+//        return allEmployees;
+//    }
+//
+//    @Override
+//    public void saveEmployee(Employee employee) {
+//
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        //this method add new employee if id=0 and update exist employee in another case
+//        session.saveOrUpdate(employee);
+//    }
+//
+//    @Override
+//    public Employee getEmployee(int id) {
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        Employee employee =session.get(Employee.class, id);
+//        return employee;
+//    }
+//
+//    @Override
+//    public void deleteEmployee(int id) {
+//
+//        Session session = entityManager.unwrap(Session.class);
+//
+////        //option without Query
+////        Employee employee =session.get(Employee.class, id);
+////        session.delete(employee);
+//
+//       //option with Query and setParameter
+//        Query <Employee> query = session
+//                .createQuery("delete from Employee where id=:employeeId");
+//        query.setParameter("employeeId", id);
+//        query.executeUpdate();
+//
+//    }
+//}
+
+//**************************************************************************
+//realization of DAOImpl without direct using of Hibernate
+
 package com.mmm.spring.springboot.springboot_rest.dao;
 
 import com.mmm.spring.springboot.springboot_rest.entity.Employee;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
-//this version of DAOImpl uses hibernate realization of JPA
 @Repository
 public class EmployeeDAOImpl implements EmployeeDAO{
 
@@ -19,51 +91,36 @@ public class EmployeeDAOImpl implements EmployeeDAO{
     @Override
 //    @Transactional // this annotation used here if we have NO service layer
     public List<Employee> getAllEmployees() {
-        Session session = entityManager.unwrap(Session.class);
-//        //all in one statement
-//        List <Employee> allEmployees = session
-//                .createQuery("from Employee ", Employee.class)
-//                .getResultList();
 
-        //the same as above in two statements
-        Query <Employee> query = session
-                .createQuery("from Employee ", Employee.class);
+        Query query = entityManager.createQuery("from Employee ");
         List <Employee> allEmployees = query
                 .getResultList();
         return allEmployees;
+
     }
 
     @Override
     public void saveEmployee(Employee employee) {
 
-        Session session = entityManager.unwrap(Session.class);
-
-        //this method add new employee if id=0 and update exist employee in another case
-        session.saveOrUpdate(employee);
+    Employee newEmployee = entityManager.merge(employee);
+    employee.setId(newEmployee.getId());
     }
 
     @Override
     public Employee getEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
 
-        Employee employee =session.get(Employee.class, id);
+        Employee employee =entityManager.find(Employee.class, id);
         return employee;
     }
 
     @Override
     public void deleteEmployee(int id) {
 
-        Session session = entityManager.unwrap(Session.class);
-
-//        //option without Query
-//        Employee employee =session.get(Employee.class, id);
-//        session.delete(employee);
-
-       //option with Query and setParameter
-        Query <Employee> query = session
+        Query query = entityManager
                 .createQuery("delete from Employee where id=:employeeId");
         query.setParameter("employeeId", id);
         query.executeUpdate();
 
     }
 }
+
